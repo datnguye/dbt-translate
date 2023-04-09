bb41e2c1e0147a691f3bd2409cc34b049daebddb
datnguye <datnguyen.it09@gmail.com>
2023-04-09 16:36:24 +0700

    fix: remove unused code [skip ci]

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,8 +1,6 @@
-from pathlib import Path
 import shutil
 import os
 
 def copy_dbt_docs(config, **kwargs):
     site_dir = config['site_dir']
-    print(Path.cwd())
     shutil.copytree("integration_tests/target/", os.path.join(site_dir, 'dbt-docs'))
